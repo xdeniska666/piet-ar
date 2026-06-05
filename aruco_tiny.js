faa2f759ee234a528fe0f11b1fcc1d6175cc37a1
Denis666 <dlyaprogramirovaniya@gmail.com>
2026-06-05 22:07:45 +0300

    fix: переход на адаптивный threshold для детекции контуров при любом освещении

--- aruco_tiny.js
+++ aruco_tiny.js
@@ -333,7 +333,7 @@ AR.Detector = function(){
 
 AR.Detector.prototype.detect = function(image){
   CV.grayscale(image, this.grey);
-  CV.threshold(this.grey, this.thres, 128);
+  CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
 
   this.contours = CV.findContours(this.thres, this.binary);
 
