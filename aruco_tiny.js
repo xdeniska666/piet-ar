7d95c7122b24f24fd6b280313d88d96b81c338b2
Denis666 <dlyaprogramirovaniya@gmail.com>
2026-06-28 23:12:16 +0300

    Save lastReadMatrix only on successful marker match and reset before frame

--- aruco_tiny.js
+++ aruco_tiny.js
@@ -239,7 +239,7 @@ AR.Detector.prototype.detect = function(imageSrc){
   
   // Записываем реальное количество выживших кандидатов для нашей телеметрии
   window.realCandidatesCount = this.candidates.length;
-  
+
   // Разбор кандидатов в маркеры
   var markers = [];
   for (i = 0; i < this.candidates.length; ++ i){
@@ -362,8 +362,6 @@ AR.Detector.prototype.getMarker = function(imageThres, candidate) {
     }
   }
 
-  window.lastReadMatrix = bits;
-
   var realDataset = [
     [1, 1, 1, 1, 0],
     [0, 0, 1, 1, 1],
@@ -387,7 +385,9 @@ AR.Detector.prototype.getMarker = function(imageThres, candidate) {
     if (eOut < bestErrors) bestErrors = eOut;
   }
 
+  // Если маркер валидный, то фиксируем матрицу и возвращаем объект
   if (bestErrors <= 6) {
+    window.lastReadMatrix = bits;
     return new AR.Marker(100, corners);
   }
   return null;
